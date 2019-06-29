988342b03605a2af85ae5207d54028cb8b425c04
pson0007 <ping.song@monash.edu>
2019-06-29 22:25:06 +1000

    Updates

---
+++ precache-manifest.6c877c50f1ec2a5539991ffe6d9293b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd51b1aa400810738d2efc90fb11711a",
+    "url": "/ping-test/index.html"
+  },
+  {
+    "revision": "11deb03bbc83264fe0bb",
+    "url": "/ping-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "759314c782eff2632c00",
+    "url": "/ping-test/static/js/2.790236c7.chunk.js"
+  },
+  {
+    "revision": "11deb03bbc83264fe0bb",
+    "url": "/ping-test/static/js/main.18e7f366.chunk.js"
+  },
+  {
+    "revision": "0310fc1c820b44d4beec",
+    "url": "/ping-test/static/js/runtime~main.f71a86b9.js"
+  }
+]);
